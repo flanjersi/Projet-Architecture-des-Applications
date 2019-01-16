5c40ac9c7947d6fb9ff8792d9e6deec340d7394b
flanjersi <grosj83670@gmail.com>
2019-01-16 16:04:04 +0100

    modification vue et simplification bean connectedUse

--- src/fr/amu/projetADA/services/connectedUser/ConnectedUserBean.java
+++ src/fr/amu/projetADA/services/connectedUser/ConnectedUserBean.java
@@ -9,27 +9,9 @@ import fr.amu.projetADA.services.person.PersonManager;
 @Stateful(name = "connectedUser")
 public class ConnectedUserBean implements ConnectedUser {
 
-	@EJB
-	private PersonManager personManager;
 	
 	private Person personlogged;
 	
-	@Override
-	public boolean login(String login, String pwd) {
-		Person p = personManager.findByEmailAndPassword(login, pwd);
-		
-		if(p == null)
-			return false;
-		
-		personlogged = p;
-		return true;
-	}
-
-	@Override
-	public void logout() {
-		personlogged = null;
-	}
-
 	@Override
 	public void setPersonLogged(Person person) {
 		this.personlogged = person;
