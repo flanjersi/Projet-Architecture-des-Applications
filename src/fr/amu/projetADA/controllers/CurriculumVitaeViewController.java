06268e0a191ea431f923f2feada3c7ffcdbedb90
ramzi-guellil <brixyes@gmail.com>
2019-02-09 09:56:00 +0100

    fonctions recherches 2

--- src/fr/amu/projetADA/controllers/CurriculumVitaeViewController.java
+++ src/fr/amu/projetADA/controllers/CurriculumVitaeViewController.java
@@ -62,12 +62,14 @@ public class CurriculumVitaeViewController implements Serializable{
 					List<CurriculumVitae> concatCvs = new ArrayList<>();
 					concatCvs.addAll(cvsByActivity);
 					
-					for(int i=0;i<cvsByTitle.size();i++)
-						for(int j=0;j<cvsByActivity.size();i++)
+					for(int i=0;i<cvsByTitle.size();i++) {
+						for(int j=0;j<cvsByActivity.size();i++) 
 							if(cvsByTitle.get(i).equals(cvsByActivity.get(j)))
 								{i++; break;}
-							else concatCvs.add(cvsByTitle.get(i)); 
-		
+						
+							 concatCvs.add(cvsByTitle.get(i)); 
+					}
+					//concatCvs.addAll(cvsByTitle); 
 					//curriculumsVitae.setRowCount((int) curriculumVitaeManager.findCvByActivity(filter,pageSize,first));
 					
 					setNbData(concatCvs.size());
